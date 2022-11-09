9daac5fd93958937b1d67b8715382d1aeaed882a
aboudyabdulkarim <aboudyabdulkarim@gmail.com>
2022-11-09 12:14:52 -0800

    js2-4

---
+++ javascript-basics-02/step 4/index.js
@@ -0,0 +1,9 @@
+const name = document.getElementById('name')
+const surname = document.getElementById('surname')
+const city = document.getElementById('city')
+
+function reset() {
+    name.value = ''
+    surname.value = ''
+    city.value = ''
+}
